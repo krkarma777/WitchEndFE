e6b36814a0a448a2b28af438fb0eb68d5e4234be
krkarma777 <krkarma777@gmail.com>
2024-04-23 10:50:56 +0900

    Add LoginForm and configure image handling in router
    - Added LoginForm.vue component to the routes in vue-router.
    - Configured webpack in vue-router to handle image files using url-loader, setting up Base64 encoding for images smaller than 10KB and defining output and public paths for larger images.

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-// 각각의 경로에 해당하는 컴포넌트를 import 합니다.
 import UserForm from '../views/UserForm.vue';
+import LoginForm from '../views/LoginForm.vue';
 
-// createRouter 함수를 사용하여 Vue Router를 생성합니다.
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: UserForm },
-  ]
+    { path: '/login', component: LoginForm }
+  ],
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+        .loader('url-loader')
+        .tap(options => Object.assign(options, {
+          limit: 10240, // 10KB 이하의 파일을 Base64 인코딩
+          name: '[name].[hash:8].[ext]',
+          outputPath: 'assets/images/', // 출력 경로
+          publicPath: 'assets/images/'  // HTML에서 참조될 때의 경로
+        }));
+  }
 });
 
 export default router;
